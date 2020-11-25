fe4af724ae2ad6c7a67899a5e007abb4f2215e64
nehledit <nehalg2@gmail.com>
2020-11-25 09:42:20 +0530

    Updates

---
+++ precache-manifest.36a04478b77522724d4151a6e8f70f7d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4db020382a803905c2ca1fa13d8cac58",
+    "url": "/Reactconfusion/index.html"
+  },
+  {
+    "revision": "f644638f00e05e41bc9e",
+    "url": "/Reactconfusion/static/css/2.b65e54f5.chunk.css"
+  },
+  {
+    "revision": "7fc8ff39536e1f60a994",
+    "url": "/Reactconfusion/static/css/main.533e19f4.chunk.css"
+  },
+  {
+    "revision": "f644638f00e05e41bc9e",
+    "url": "/Reactconfusion/static/js/2.9a5dfa15.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Reactconfusion/static/js/2.9a5dfa15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fc8ff39536e1f60a994",
+    "url": "/Reactconfusion/static/js/main.e1c541d5.chunk.js"
+  },
+  {
+    "revision": "327991ef861a36618ee4",
+    "url": "/Reactconfusion/static/js/runtime-main.0fad1114.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Reactconfusion/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Reactconfusion/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Reactconfusion/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Reactconfusion/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Reactconfusion/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
